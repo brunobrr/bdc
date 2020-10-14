94ee9e1d688a98ce5654486be5b3c7e8c591c2b6
Bruno R. Ribeiro <ribeiro.brr@gmail.com>
2020-10-14 17:35:08 -0300

    atualizar script utilizado para amostrar as bases de dados

--- sample_datasets.R
+++ sample_datasets.R
@@ -16,15 +16,13 @@ ipak(c("tidyverse", "data.table", "vroom", "data.table"))
 
 # atlantic epiphytes - occurence records
 atlant_occur <-
-  vroom(
-    "Data_raw/at_epiphytes/DataS1_Occurrence.txt",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+  read_delim("Data_raw/at_epiphytes/DataS1_Occurrence.txt", 
+             "\t", escape_double = FALSE, trim_ws = TRUE)
 
 # Atlantic epiphytes - abundance data
 atlant_abund <-
-  vroom(
-    "Data_raw/at_epiphytes/DataS1_Abundance.txt",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+  read_delim("Data_raw/at_epiphytes/DataS1_Abundance.txt", 
+             "\t", escape_double = FALSE, trim_ws = TRUE)
 
 # Filter collumn names according to collumn names of "atlantic_occur"
 names <- which(colnames(atlant_abund) %in% colnames(atlant_occur))
@@ -48,10 +46,7 @@ data.table::fwrite(at_epiphytes_s, "Input_files/at_epiphytes.csv")
 
 
 # bien --------------------------------------------------------------------
-bien <-
-  vroom(
-    "Data_raw/bien/BIEN_Brazil-UTF8.csv",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+bien <- read_csv("Data_raw/bien/BIEN_Brazil-UTF8.csv")
 
 # Select a sub-sample
 bien_s <- sample_n(bien, 10000)
@@ -62,10 +57,7 @@ data.table::fwrite(bien_s, "Input_files/bien.csv")
 
 
 # dryflor -----------------------------------------------------------------
-dryflor <-
-  vroom(
-    "Data_raw/dryflor/records.csv",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+dryflor <- read_csv("Data_raw/dryflor/records.csv")
 
 # Select a sub-sample
 dryflor_s <- sample_n(dryflor, 10000)
@@ -76,10 +68,8 @@ data.table::fwrite(dryflor_s, "Input_files/dryflor.csv")
 
 
 # gbif --------------------------------------------------------------------
-gbif <-
-  vroom(
-    "Data_raw/gbif/0069607-200613084148143.csv",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+gbif <- read_delim("Data_raw/gbif/0069607-200613084148143.csv", 
+                   "\t", escape_double = FALSE, trim_ws = TRUE)
 
 # Select a sub-sample
 gbif_s <- sample_n(gbif, 10000)
@@ -90,29 +80,23 @@ data.table::fwrite(gbif_s, "Input_files/gbif.csv")
 
 
 # icmbio ------------------------------------------------------------------
-icmbio <-
-  vroom(
-    "Data_raw/icmbio/portalbio_export_28-09-2020-17-41-57.csv",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+icmbio <- read_delim("Data_raw/icmbio/portalbio_export_28-09-2020-17-41-57.csv", 
+                     ";", escape_double = FALSE, trim_ws = TRUE)
+
 
 # Select a sub-sample
 icmbio_s <- sample_n(icmbio, 10000)
 
 # Save file
-data.table::fwrite(icmbio_s, "Input_files/icmbio.csv")
+data.table::fwrite(gbif_s, "Input_files/gbif.csv")
+
 
 
 
 # idigibio ----------------------------------------------------------------
-idigbio_brazil <-
-  vroom(
-    "Data_raw/idigbio/c7525055-fd26-4c13-9859-dc298d57ff09.zip",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+idigbio_brazil <- read_csv("Data_raw/idigbio/occurrence_c7525055-fd26-4c13-9859-dc298d57ff09.csv")
 
-idigbio_brasil <-
-  vroom(
-    "Data_raw/idigbio/eee7a8c2-aa07-4a83-b15b-ee02d260e369.zip",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+idigbio_brasil <- read_csv("Data_raw/idigbio/occurrence_eee7a8c2-aa07-4a83-b15b-ee02d260e369.csv")
 
 # Merge datasets
 idigbio <- rbind(idigbio_brazil, idigbio_brasil)
@@ -125,16 +109,12 @@ data.table::fwrite(idigbio_s, "Input_files/idigbio.csv")
 
 
 # neotroptree -------------------------------------------------------------
-neotroptree <-
-  vroom(
-    "Data_raw/neotroptree/species-lat-long_long-table.csv",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+neotroptree <- read_csv("Data_raw/neotroptree/species-lat-long_long-table.csv")
 
 neotroptree_s <- sample_n(neotroptree, 10000)
 
 # Save file
-data.table::fwrite(neotroptree_s, "Input_files/neotroptree.csv").csv
-
+data.table::fwrite(neotroptree_s, "Input_files/neotroptree.csv")
 
 
 # sibbr -------------------------------------------------------------------
@@ -153,9 +133,8 @@ data.table::fwrite(sibbr_s, "Input_files/sibbr.csv")
 
 # specieslink -------------------------------------------------------------
 specieslink <-
-  vroom(
-    "Data_raw/specieslink/speciesLink_all_97889_20200925235952.txt",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+  read_delim("Data_raw/specieslink/speciesLink_all_97889_20200925235952.txt", 
+             "\t", escape_double = FALSE, trim_ws = TRUE)
 
 specieslink_s <- sample_n(specieslink, 10000)
 
@@ -166,10 +145,7 @@ data.table::fwrite(specieslink_s, "Input_files/specieslink.csv")
 
 
 # at_trees ----------------------------------------------------------------
-at_trees <-
-  vroom(
-    "Data_raw/at_trees/Zwiener_2017.csv",
-    locale = locale(encoding = "UTF-8"), trim_ws = T)
+at_trees <- read_csv("Data_raw/at_trees/Zwiener_2017.csv")
 
 at_trees_s <- sample_n(at_trees, 10000)
 
