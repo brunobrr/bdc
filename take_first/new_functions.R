73ad0322251cd2dd9b0a24a5732c0e6c24812591
lucas-jardim <lucas.ljardim9@gmail.com>
2020-11-26 12:41:35 -0300

    take_first tests

---
+++ take_first/new_functions.R
@@ -0,0 +1,66 @@
+min_get_names <- function (id, db = getOption("taxadb_default_provider", "itis"), 
+          version = taxadb:::latest_version(), format = c("guess", "prefix", 
+                                                 "bare", "uri"), taxadb_db = taxadb::td_connect()) 
+{
+  format <- match.arg(format)
+  n <- length(id)
+  prefix_ids <- switch(format, prefix = id, taxadb:::as_prefix(id, db))
+  df <- filter_id(prefix_ids, provider = db, version = version, 
+                  collect = FALSE, db = taxadb_db) %>% dplyr::select("scientificName", 
+                                                                     "taxonID", "sort") %>% dplyr::distinct() %>% min_take_first_duplicate() %>% 
+    dplyr::collect()
+  if (dim(df)[1] != n) {
+    stop(paste("Error in resolving possible duplicate names.", 
+               "Try the ids() function instead."), call. = FALSE)
+  }
+  df[["scientificName"]]
+}
+
+max_get_names <- function (id, db = getOption("taxadb_default_provider", "itis"), 
+                           version = taxadb:::latest_version(), format = c("guess", "prefix", 
+                                                                           "bare", "uri"), taxadb_db = taxadb::td_connect()) 
+{
+  format <- match.arg(format)
+  n <- length(id)
+  prefix_ids <- switch(format, prefix = id, taxadb:::as_prefix(id, db))
+  df <- filter_id(prefix_ids, provider = db, version = version, 
+                  collect = FALSE, db = taxadb_db) %>% dplyr::select("scientificName", 
+                                                                     "taxonID", "sort") %>% dplyr::distinct() %>% max_take_first_duplicate() %>% 
+    dplyr::collect()
+  if (dim(df)[1] != n) {
+    stop(paste("Error in resolving possible duplicate names.", 
+               "Try the ids() function instead."), call. = FALSE)
+  }
+  df[["scientificName"]]
+}
+
+min_take_first_duplicate <- function (df) 
+{
+  scientificName <- "scientificName"
+  sort <- "sort"
+  row_num <- "row_num"
+  n <- "n"
+  max_repeated <- df %>% dplyr::count(sort, sort = T) %>% utils::head(1) %>% 
+    dplyr::pull(n)
+  if (max_repeated == 1) 
+    return(df)
+  df %>% dplyr::arrange(scientificName) %>% dplyr::mutate(row_num = dplyr::row_number()) %>% 
+    dplyr::group_by(sort) %>% dplyr::filter(row_num == min(row_num, na.rm = TRUE)) %>% 
+    dplyr::ungroup() %>% dplyr::arrange(sort)
+}
+
+
+max_take_first_duplicate <- function (df) 
+{
+  scientificName <- "scientificName"
+  sort <- "sort"
+  row_num <- "row_num"
+  n <- "n"
+  max_repeated <- df %>% dplyr::count(sort, sort = T) %>% utils::head(1) %>% 
+    dplyr::pull(n)
+  if (max_repeated == 1) 
+    return(df)
+  df %>% dplyr::arrange(scientificName) %>% dplyr::mutate(row_num = dplyr::row_number()) %>% 
+    dplyr::group_by(sort) %>% dplyr::filter(row_num == max(row_num, na.rm = TRUE)) %>% 
+    dplyr::ungroup() %>% dplyr::arrange(sort)
+}
---
+++ take_first/test_codes.R
@@ -0,0 +1,28 @@
+library(taxadb)
+library(dplyr)
+library(duckdb)
+library(flora)
+
+
+# Download fb database ----------------------------------------------------
+
+td_create("fb")
+
+
+# Get species data --------------------------------------------------------
+
+x <- filter_name("Leporinus reinhardti", "fb")
+
+
+# Return duplicates -------------------------------------------------------
+
+dup <- filter_id(x$acceptedNameUsageID, "fb")
+right_name <- dup %>% filter(taxonomicStatus == "accepted") %>% pull(scientificName)
+
+# Testing take_first ------------------------------------------------------
+
+
+max_name <- max_get_names(x$acceptedNameUsageID, "fb")
+min_name<- min_get_names(x$acceptedNameUsageID, "fb")
+
+c(max_name, min_name) == right_name
