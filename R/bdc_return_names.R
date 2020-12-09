3cea970fad673725c149de1078d3166d38dac426
lucas-jardim <lucas.ljardim9@gmail.com>
2020-12-09 11:17:42 -0300

    optimized suggest_names

--- R/bdc_return_names.R
+++ R/bdc_return_names.R
@@ -1,32 +1,24 @@
-return_names <- function(max.distance, max.dist, distance, species.first.letter, uncertain, ident, return.na){
-if (max.dist >= max.distance) {
+return_names <- function(taxon, max.distance, species.first.letter){
+
+  out <- stringdist::stringdist(taxon, species.first.letter)
+  #p_max <- pmax(nchar(taxon), nchar(species.first.letter$scientificName))
+  #distance <- 1 - (out/p_max)
+  #max.dist <- max(distance, na.rm = TRUE)
+#  min_dist_name <- species.first.letter[out == min(out)][1]
+  min_dist_name <- species.first.letter[out == sort(out, decreasing = FALSE)[1]][1]
+  sorted <- sort(c(nchar(taxon), nchar(min_dist_name)))
+  max.dist <- sorted[1]/sorted[2]
   
-  if (length(ident) == 0L) {
-    dis <- max(distance, na.rm = TRUE)
-    res <- species.first.letter$scientificName[distance == dis][1]
+  if (max.dist >= max.distance) {
     
-    if (length(uncertain) == 0L) {
-      
-      return(c(res, dis))
-    }
-    else {
-      res <- unlist(strsplit(res, " "))
-      return(c(paste(res[1], uncertain, res[2:length(res)]), dis))
-    }
+    return(data.frame(suggested = min_dist_name, distance = max.dist))
+    
   }
   else {
-    paste(species.first.letter$scientificName[distance == max(distance, 
-                                                              na.rm = TRUE)][1], ident, sep = "")
+    
+    return(data.frame(suggested = NA, distance = max.dist))
+    
   }
-}
-else {
-  if (return.na) {
-    c(NA, "0") # Mudar o valor de 0 para o real
-  }
-  else {
-    taxon.orig
-  }
-}
 }
 
 
