b2a7637c8e5d8ca73adf727aee5bd46e133b7961
lucas-jardim <lucas.ljardim9@gmail.com>
2020-12-09 11:27:10 -0300

    changing taxon to sci_name

--- R/bdc_return_names.R
+++ R/bdc_return_names.R
@@ -1,13 +1,12 @@
-return_names <- function(taxon, max.distance, species.first.letter){
+return_names <- function(sci_name, max.distance, species.first.letter){
 
-  out <- stringdist::stringdist(taxon, species.first.letter)
-  #p_max <- pmax(nchar(taxon), nchar(species.first.letter$scientificName))
+  out <- stringdist::stringdist(sci_name, species.first.letter)
+  #p_max <- pmax(nchar(sci_name), nchar(species.first.letter$scientificName))
   #distance <- 1 - (out/p_max)
   #max.dist <- max(distance, na.rm = TRUE)
 #  min_dist_name <- species.first.letter[out == min(out)][1]
   min_dist_name <- species.first.letter[out == sort(out, decreasing = FALSE)[1]][1]
-  sorted <- sort(c(nchar(taxon), nchar(min_dist_name)))
-  max.dist <- sorted[1]/sorted[2]
+  sorted <- sort(c(nchar(sci_name), nchar(min_dist_name)))
   
   if (max.dist >= max.distance) {
     
--- R/bdc_suggest_names_taxadb.R
+++ R/bdc_suggest_names_taxadb.R
@@ -1,7 +1,7 @@
 
 #' Title: Function used to standardized taxonomic names using taxadb R packpage. This functions is a modification of get.taxa function (flora package) inserted in get_names function (taxadb package) for allowing fuzzy matching.
 #'
-#' @param taxon 
+#' @param sci_name 
 #' @param max.distance 
 #' @param provider 
 #'
@@ -10,26 +10,26 @@
 #'
 #' @examples
 bdc_suggest_names_taxadb <-
-  function (taxon,
+  function (sci_name,
             max.distance = 0.75,
             provider
             
   ) {
     
-    first.letter <- unique(sapply(taxon, function(i) strsplit(i, "")[[1]][1], USE.NAMES = FALSE))
+    first.letter <- unique(sapply(sci_name, function(i) strsplit(i, "")[[1]][1], USE.NAMES = FALSE))
    # species.first.letter <- suppressWarnings(taxadb::name_starts_with(first.letter, provider = provider))[, "scientificName"]
     species.first.letter <- taxadb::taxa_tbl(provider) %>% pull(scientificName) %>% grep(paste0("^", first.letter, collapse = "|"), .,value = TRUE)
-    sug_dat <- data.frame(suggested = character(length(taxon)), distance = numeric(length(taxon)))
+    sug_dat <- data.frame(suggested = character(length(sci_name)), distance = numeric(length(sci_name)))
     
-    for(i in seq_along(taxon)){
+    for(i in seq_along(sci_name)){
       
-      sug_dat[i, ]  <- return_names(taxon[i], max.distance, species.first.letter)
+      sug_dat[i, ]  <- return_names(sci_name[i], max.distance, species.first.letter)
       
     }
     
     sug_dat
     
-    #return(sapply(taxon, FUN = return_names, max.distance, species.first.letter, USE.NAMES =FALSE))
+    #return(sapply(sci_name, FUN = return_names, max.distance, species.first.letter, USE.NAMES =FALSE))
     
   }
 
