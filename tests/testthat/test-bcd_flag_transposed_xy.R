e0da5d59102b2166ac10b7889e8d6ddf880cd12a
Geiziane <geites@gmail.com>
2020-12-10 23:23:53 -0300

    add testes and minor changes for some functions

---
+++ tests/testthat/test-bcd_flag_transposed_xy.R
@@ -0,0 +1,95 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+
+
+
+bcd_flag_transposed_xy <- function(data) {
+  
+  minimum_colnames <- c("database_id", "scientificName", "decimalLongitude", "decimalLatitude")
+  check_minimum_colnames <-
+    data %>%
+    dplyr::select(minimum_colnames) %>% {
+      names(.) %in% minimum_colnames
+    }
+  
+  if (sum(check_minimum_colnames) < length(check_minimum_colnames)) {
+    stop(
+      "Your data does not have the minimum default columns names: ",
+      paste(minimum_colnames, collapse = ", "), call. = FALSE
+    )
+  }
+  
+  
+  
+  
+  # load auxiliar data
+  wiki_cntr <- bdc_get_wiki_country() # get country names from Wikipedia
+  worldmap <- bdc_get_world_map()  # 
+  
+  # standardize the name of countries
+  standard_country_names <-
+    bdc_standardize_country(
+      data = data,
+      country = "country",
+      country_names_db = wiki_cntr
+    )
+  
+  data <-
+    data %>%
+    dplyr::left_join(standard_country_names, by = c("country" = "cntr_original"))
+  
+  # Correct latitude and longitude transposed
+  corrected_coordinates <-
+    bdc_correct_coordinates(
+      data = data,
+      x = "decimalLongitude",
+      y = "decimalLatitude",
+      sp = "scientificName",
+      id = "database_id",
+      cntr_iso2 = "cntr_iso2c",
+      world_poly = worldmap,
+      world_poly_iso = "iso2c"
+    )
+  
+  rows_to_remove <-
+    corrected_coordinates %>%
+    dplyr::pull(database_id)
+  
+  rows_to_insert <-
+    corrected_coordinates %>%
+    # remove columns with coordinates transposed
+    dplyr::select(-decimalLatitude, -decimalLongitude) %>%
+    # new columns coordinates with the corrected info
+    dplyr::rename(decimalLatitude = decimalLatitude_modified,
+                  decimalLongitude = decimalLongitude_modified) %>%
+    # flag all of them
+    dplyr::mutate(transposed_xy = TRUE)
+  
+  data <-
+    data %>%
+    # remove wrong coordinates
+    dplyr::filter(!database_id %in% rows_to_remove) %>%
+    # flag no issued rows as FALSE
+    dplyr::mutate(transposed_xy = FALSE) %>%
+    # add corrected coordinates
+    dplyr::bind_rows(rows_to_insert)
+  
+  # save issued coordinates
+  message("Saving issued coordinates in Output/Check/01_prefilter_transposed_coordinates.csv")
+  
+  corrected_coordinates %>%
+    dplyr::select(
+      database_id,
+      scientificName,
+      dplyr::contains("decimal"),
+      locality,
+      stateProvince,
+      cntr_suggested
+    ) %>%
+    readr::write_csv(here::here("Output", "Check", "01_prefilter_transposed_coordinates.csv"))
+  
+  return(data)
+  
+}
--- tests/testthat/test-bdc_rem_taxo_unc.R
+++ tests/testthat/test-bdc_rem_taxo_unc.R
@@ -1,93 +1,85 @@
-Context("test_ flag_uncertainty_taxonomy")
+
+context("rem_taxo_unc")
+
 
 n_confer<- c("Jorge Anibal cf", "joao cf.", "joao CF","joao CF.", "João Cf","João cF.", "cf joao")   
 
 test_that("confer", {
-  res<-bdc_flag_taxo_unc(n_confer)
-  res<-as.logical(res[,2])
-  expect_equal(res, rep(FALSE,length(n_confer)))
+  res<-bdc_rem_taxo_unc(n_confer)
+  expect_equal(res, c("Jorge Anibal", "joao","joao","joao", "João", "João", "joao" ))
 })
 
 n_afinis<-c("Jorge Anibal aff", "joao aff.", "joao AFF", "João AFF.","João AfF.", "João aFF.", "João aFF.","aff João")              
 
 test_that("afinis", {
-  res<-bdc_flag_taxo_unc(n_afinis)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_afinis))))
+  res<-bdc_rem_taxo_unc(n_afinis)
+  expect_equal(res, c("Jorge Anibal", "joao","joao","João", "João", "João", "João","João" ))
 })
 
 n_complex<- c("Jorge Anibal complex", "joao COMPLEX", "João Complexo.", "João COMplex","complex João")
 
 test_that("complex", {
-  res<-bdc_flag_taxo_unc(n_complex)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_complex))))
+  res<-bdc_rem_taxo_unc(n_complex)
+  expect_equal(res, c("Jorge Anibal", "joao", "João .", "João","João"))
 })
-
 
 n_gen<- c("Jorge Anibal gen", "joao GEN", "João gen.", "João gen ","gen João"," João GEn")
 
 test_that("gen", {
-  res<-bdc_flag_taxo_unc(n_gen)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_gen))))
+  res<-bdc_rem_taxo_unc(n_gen)
+  expect_equal(res, c("Jorge Anibal", "joao", "João", "João","João","João"))
 })
 
 n_sp<-   c("Jorge Anibal sp", "joao sp.", "João ssp", "João ssp.","sp João", "joão SSP", "joão SP." )              
 
 test_that("sp", {
-  res<-bdc_flag_taxo_unc(n_sp)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_sp))))
+  res<-bdc_rem_taxo_unc(n_sp)
+  expect_equal(res, c( "Jorge Anibal", "joao", "João", "João", "João", "joão", "joão"))
 })
 
 n_incerta<-  c("Jorge Anibal inc", "joao inc.", "João inc ", "João INC","inc João", "joão iNc")
 
 test_that("incerta", {
-  res<-bdc_flag_taxo_unc(n_incerta)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_incerta))))
+  res<-bdc_rem_taxo_unc(n_incerta)
+  expect_equal(res, c("Jorge Anibal", "joao","João","João","João", "joão"))
 })
 
 n_inquirenda<- c("Jorge Anibal inq", "joao inq.", "João inq ", "João INQ","inq João", "joão iNq")
 
 test_that("inquirenda", {
-  res<-bdc_flag_taxo_unc(n_inquirenda)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_inquirenda))))
+  res<-bdc_rem_taxo_unc(n_inquirenda)
+  expect_equal(res, c("Jorge Anibal", "joao", "João", "João", "João","joão"))
 })
 
 
 n_indet<- c("Jorge Anibal ind", "joao ind.", "João ind ", "João IND","ind João", "joão iNd","Jorge Anibal indet", "joao indet.", "João indet ", "João INDET","indet João", "joão iNdeT")
 
 test_that("indet", {
-  res<-bdc_flag_taxo_unc(n_indet)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_indet))))
+  res<-bdc_rem_taxo_unc(n_indet)
+  expect_equal(res, c( "Jorge Anibal", "joao","João", "João", "João", "joão",        
+                       "Jorge Anibal", "joao", "João", "João", "João","joão"))
 })
 
 n_nova<- c("Jorge Anibal nov", "joao nov.", "João nov ", "João NOV","nov João", "joão NOv")
 
 test_that("nova", {
-  res<-bdc_flag_taxo_unc(n_nova)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_nova))))
+  res<-bdc_rem_taxo_unc(n_nova)
+  expect_equal(res, c("Jorge Anibal", "joao","João", "João", "João","joão"))
 })
 
 
 n_prox<- c("Jorge Anibal prox", "joao prox.", "João prox ", "João PROX","prox João", "joão pRox","Jorge Anibal nr", "joao nr.", "João nr ", "João NR","nr João", "joão Nr")
 
 test_that("prox", {
-  res<-bdc_flag_taxo_unc(n_prox)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_prox))))
+  res<-bdc_rem_taxo_unc(n_prox)
+  expect_equal(res, c( "Jorge Anibal", "joao","João", "João","João","joão",        
+                       "Jorge Anibal", "joao","João", "João", "João","joão"))
 })
 
 n_stet<-  c("Jorge Anibal stet", "joao stet.", "João stet ", "João STET","stet João", "joão SteT")
 
 test_that("stet", {
-  res<-bdc_flag_taxo_unc(n_stet)
-  res<-as.logical(res[,2])
-  expect_equal(res, c(rep(FALSE, length(n_stet))))
+  res<-bdc_rem_taxo_unc(n_stet)
+  expect_equal(res, c("Jorge Anibal", "joao","João", "João", "João","joão"))
 })
 
