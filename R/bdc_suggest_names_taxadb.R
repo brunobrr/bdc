4ce550403908df70120e203dc025f579d40d0194
lucas-jardim <lucas.ljardim9@gmail.com>
2021-02-09 13:05:14 -0300

    substitute . into _ in arguments and functions

--- R/bdc_suggest_names_taxadb.R
+++ R/bdc_suggest_names_taxadb.R
@@ -1,9 +1,9 @@
 #' Title: Suggest valid names based on taxadb R package.
 #'
-#' This function is a modification of suggest.name function (flora package) accessing taxadb database for fuzzy matching.
+#' This function is a modification of suggest_name function (flora package) accessing taxadb database for fuzzy matching.
 #'
 #' @param sci_name character, containing scientific to be searched in taxadb database. The function does not clean names (eg.: infraspecific names), so this procedure should be done previously.
-#' @param max.distance numeric,  a value between 0 and 1 specifying the minimum distance between the scientific names and names suggested by a fuzzy matching. Values close to 1 indicate that only a few differences between scientific names and name suggested are allowed. Default is 0.9.
+#' @param max_distance numeric,  a value between 0 and 1 specifying the minimum distance between the scientific names and names suggested by a fuzzy matching. Values close to 1 indicate that only a few differences between scientific names and name suggested are allowed. Default is 0.9.
 #' @param provider A database where the valid and suggest names should be searched. The options are those provided by taxadb package.
 #' @param rank_name a character string of taxonomic scientific name (e.g. "Plantae"). Default is NULL
 #' @param rank a character string containing a taxonomic rank name (e.g. "kingdom"). Default is NULL
@@ -18,7 +18,7 @@
 #'
 bdc_suggest_names_taxadb <-
   function(sci_name,
-           max.distance = suggestion.distance,
+           max_distance = suggestion_distance,
            provider = db,
            rank_name = NULL,
            rank = NULL,
@@ -26,31 +26,31 @@ bdc_suggest_names_taxadb <-
            ncores = 2) {
     
     # Get first letter of all scientific names
-    first.letter <-
+    first_letter <-
       unique(sapply(sci_name, function(i) {
         strsplit(i, "")[[1]][1]
       },
       USE.NAMES = FALSE
       ))
 
-    first.letter <- base::toupper(first.letter)
+    first_letter <- base::toupper(first_letter)
 
     # Should taxonomic database be filter according to a taxonomic rank name?
     if (!is.null(rank_name) & !is.null(rank)) {
-      species.first.letter <-
+      species_first_letter <-
         taxadb::taxa_tbl(provider) %>%
         dplyr::filter(., .data[[rank]] == rank_name) %>%
         dplyr::pull(scientificName) %>%
-        grep(paste0("^", first.letter, collapse = "|"), ., value = TRUE)
+        grep(paste0("^", first_letter, collapse = "|"), ., value = TRUE)
     } else if (is.null(rank_name) & !is.null(rank)) {
       message("Please, provide both 'rank_name' and 'rank' arguments")
     } else if (!is.null(rank_name) & is.null(rank)) {
       message("Please, provide both 'rank_name' and 'rank' arguments")
     } else {
-      species.first.letter <-
+      species_first_letter <-
         taxadb::taxa_tbl(provider) %>%
         pull(scientificName) %>%
-        grep(paste0("^", first.letter, collapse = "|"), ., value = TRUE)
+        grep(paste0("^", first_letter, collapse = "|"), ., value = TRUE)
     }
 
 
@@ -63,7 +63,7 @@ bdc_suggest_names_taxadb <-
       sug_dat <-
         foreach(i = sci_name,
                 .combine = rbind, .export = "bdc_return_names") %dopar% {
-          bdc_return_names(i, max.distance, species.first.letter)
+          bdc_return_names(i, max_distance, species_first_letter)
         } # end foreach
       parallel::stopCluster(cl) # stop cluster
     } else {
@@ -76,7 +76,7 @@ bdc_suggest_names_taxadb <-
 
       for (i in seq_along(sci_name)) {
         sug_dat[i, ] <-
-          bdc_return_names(sci_name[i], max.distance, species.first.letter)
+          bdc_return_names(sci_name[i], max_distance, species_first_letter)
       }
     }
     return(sug_dat)
