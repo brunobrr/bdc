6ee27e47c0e60b76fafdcdf3d8237fa13ddb526f
lucas-jardim <lucas.ljardim9@gmail.com>
2021-02-09 13:04:43 -0300

    substitute . into _ in arguments and functions

--- R/bdc_return_names.R
+++ R/bdc_return_names.R
@@ -3,27 +3,27 @@
 #' This function looks for the closest name in a vector of names and returns the string distances calculated by fuzzy matching.
 #'
 #' @param sci_name A character vector with a single name.
-#' @param max.distance A numeric value specifying the minimum distance between the sci_name and the names in species.first.letter. 
-#' @param species.first.letter A character vector whose distances will be calculated from sci_name.
+#' @param max_distance A numeric value specifying the minimum distance between the sci_name and the names in species_first_letter. 
+#' @param species_first_letter A character vector whose distances will be calculated from sci_name.
 #'
 #' @return This function returns a data.frame whose first column is the closest name and the second column is the distance between the sci_name and the closest name.   
 #' @export
 #'
 #' @examples
-#' bdc_return_names("Cebus apela", max.distance = 0.75, species.first.letter = c("Cebus apella", "Puma concolar"))
+#' bdc_return_names("Cebus apela", max_distance = 0.75, species_first_letter = c("Cebus apella", "Puma concolar"))
 #' 
 
 
-bdc_return_names <- function(sci_name, max.distance, species.first.letter) {
-  out <- stringdist::stringdist(sci_name, species.first.letter)
-  min_dist_name <- species.first.letter[out == sort(out, decreasing = FALSE)[1]][1]
+bdc_return_names <- function(sci_name, max_distance, species_first_letter) {
+  out <- stringdist::stringdist(sci_name, species_first_letter)
+  min_dist_name <- species_first_letter[out == sort(out, decreasing = FALSE)[1]][1]
   sorted <- sort(c(nchar(sci_name), nchar(min_dist_name)))
-  max.dist <- 1 - (min(out) / sorted[2])
+  max_dist <- 1 - (min(out) / sorted[2])
 
-  if (max.dist >= max.distance) {
-    return(data.frame(original = sci_name, suggested = min_dist_name, distance = round(max.dist, 2)))
+  if (max_dist >= max_distance) {
+    return(data.frame(original = sci_name, suggested = min_dist_name, distance = round(max_dist, 2)))
   }
   else {
-    return(data.frame(original = sci_name, suggested = NA, distance = round(max.dist, 2)))
+    return(data.frame(original = sci_name, suggested = NA, distance = round(max_dist, 2)))
   }
 }
