13e2cf7f0179cf1a21d0ecf44ca5802261bf0565
Bruno R. Ribeiro <ribeiro.brr@gmail.com>
2021-02-19 20:44:02 -0200

    update script to create figures and report

--- R/bdc_aux_functions.R
+++ R/bdc_aux_functions.R
@@ -120,68 +120,167 @@ bdc_summary_col <- function(data) {
 
 # bdc_create_report -------------------------------------------------------
 bdc_create_report <- function(data, workflow_step) {
-  # First, create a table of total number of records
+  suppressMessages({  
+  # Total number of records
   if (!file_exists("data/n_records.csv")) {
     n_records <-
       data %>%
       dplyr::summarise(n = n())
+
     data.table::fwrite(n_records, "data/n_records.csv")
+
+    # Total number of records per database
+    n_record_database <-
+      data %>%
+      dplyr::mutate(
+        database_id = gsub("[[:digit:]]+", "", database_id),
+        database_id = gsub("_", "", database_id)
+      ) %>%
+      dplyr::group_by(database_id) %>%
+      dplyr::summarise(n_total = n())
+
+    data.table::fwrite(n_record_database, "data/n_record_database.csv")
+
   } else {
     n_records <- read_csv("data/n_records.csv") %>% dplyr::pull(n)
+    n_record_database <- read_csv("data/n_record_database.csv")
   }
 
   # Prefilter
   if (workflow_step == "prefilter") {
-    suppressWarnings({
-      data <-
+
+      pf <-
         data %>%
         dplyr::select(contains(".")) %>%
         dplyr::summarise_all(., .funs = sum) %>%
         t %>%
         tibble::as_tibble(rownames = "NA") %>%
         dplyr::mutate(V1 = nrow(data) - V1) %>%
-        dplyr::mutate(Perc_records_flagged = round((V1 / nrow(data) * 100), 2)) %>%
-        dplyr::rename(Name = `NA`,
+        dplyr::mutate(Perc_records_flagged =
+                        round((V1 / nrow(data) * 100), 2)) %>%
+        dplyr::rename(Test_name = `NA`,
                       Records_flagged = V1)
-    })
+    
+    pf <-
+      pf %>%
+      dplyr::mutate(Description = Test_name) %>% 
+      dplyr::mutate(
+        Description = if_else(
+          Description == ".missing_name",
+          "Records missing scientific name",
+          Description
+        ),
+        Description = if_else(Description == ".missing_xy",
+                              "Records missing coordinates",
+                              Description),
+        Description = if_else(
+          Description == ".invalid_xy",
+          "Records with invalid coordiantes",
+          Description
+        ),
+        Description = if_else(
+          Description == ".xy_provenance",
+          "Records from doubtful provenance",
+          Description
+        ),
+        Description = if_else(
+          Description == ".xy_out_country",
+          "Records outside one or multiple focal countries",
+          Description
+        ),
+        Description = if_else(Description == ".summary",
+                              "Summary of all tests", Description)
+      ) 
+    
+    pf <-
+      pf %>% 
+      dplyr::add_row()
+    
+    pf <- pf %>% dplyr::select(Description, everything())
+    
+    pf[1 + nrow(pf), 1] <-
+      paste("(*) calculated in relation to total number of records, i.e.",
+            n_records,
+            "records")
+    
+    names(pf)[4] <- "perc_number_records(*)"
+    data <- pf
   }
   
   # Taxonomy
   if (workflow_step == "taxonomy") {
-    suppressWarnings({
     names <-
       data %>%
       group_by(notes) %>%
       summarise(number_of_records = n()) %>%
-      mutate(perc_number_records = round((number_of_records / n_records) * 100, 2))
+      mutate(perc_number_records =
+               round((number_of_records / n_records) * 100, 2))
     
-    taxo_unc <-
-      data %>%
-      dplyr::select(.uncer_terms) %>%
-      dplyr::filter(.uncer_terms == FALSE) %>%
-      dplyr::group_by(.uncer_terms) %>%
-      dplyr::summarise(number_of_records = n()) %>%
-      dplyr::mutate(perc_number_records = round((number_of_records / n_records) * 100, 2)) %>%
-      dplyr::rename(notes = .uncer_terms)
+    if (".uncer_terms" %in% names(data)) {
+      taxo_unc <-
+        data %>%
+        dplyr::select(.uncer_terms) %>%
+        dplyr::filter(.uncer_terms == FALSE) %>%
+        dplyr::group_by(.uncer_terms) %>%
+        dplyr::summarise(number_of_records = n()) %>%
+        dplyr::mutate(perc_number_records = 
+                        round((number_of_records / n_records) * 100, 2)) %>%
+        dplyr::rename(notes = .uncer_terms) %>% 
+        dplyr::mutate(notes = if_else(notes == "FALSE", "taxo_uncer", notes))
+    }
     
     names <- dplyr::bind_rows(names, taxo_unc)
+      
+    names <-
+      names %>%
+      dplyr::mutate(Description = notes) %>% 
+      dplyr::mutate(
+        Description = if_else(Description == "", "valid names", Description),
+        Description = if_else(
+          Description == "was misspelled|replaced synonym",
+          "valid: was misspelled and replaced synonym",
+          Description
+        ),
+        Description = if_else(
+          Description == "taxo_uncer",
+          "check: doubtful taxonomic identification",
+          Description
+        ),
+        Description = if_else(Description == "was misspelled",
+                        "valid: was misspelled", Description),
+        Description = if_else(Description == "not found",
+                        "invalid: not found", Description),
+        Description = if_else(Description == "|replaced synonym",
+                        "valid: replaced synonym", Description),
+        Description = if_else(
+          Description == "|check +1 accepted",
+          "check: more than one accepted name found",
+          Description
+        ),
+        Description = if_else(
+          Description == "|check no accepted name",
+          "check: no accepted name found",
+          Description
+        )
+      ) 
     
-    names[1, 1] <- "valid names"
-    names[2, 1] <- "check: more than one accepted name found"
-    names[3, 1] <- "check: no accepted name found"
-    names[4, 1] <- "valid: replaced synonym, "
-    names[5, 1] <- "invalid: not found"
-    names[6, 1] <- "valid: was misspelled"
-    names[7, 1] <- "valid: was misspelled and replaced synonym"
-    names[8, 1] <- "check: doubtful taxonomic identification"
-    names[9, 1] <- ""
+    names <-
+      names %>% 
+      dplyr::add_row()
     
-    names[10, 1] <- paste("* calculated in relation to total number of records, i.e.", n_records,"records")
+    names <- names %>% dplyr::select(Description, everything())
     
-    names(names)[3] <- "*perc_number_records"
-    })
-  return(names)
-}
+    names[1 + nrow(names), 1] <-
+      paste("(*) calculated in relation to total number of records, i.e.",
+            n_records,
+            "records")
+    
+    names(names)[4] <- "perc_number_records(*)"
+    data <- names
+  }
+    
+  return(data)
+  })
 }
 
 # bdc_filter_out_flags ----------------------------------------------------
