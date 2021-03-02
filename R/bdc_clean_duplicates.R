7f05437fad3f2aea52c4847214921fab763d826a
Bruno R. Ribeiro <ribeiro.brr@gmail.com>
2021-03-02 08:57:46 -0300

    fixing a error in get_taxa_db; standardizing notes; fixing bug when filtering by a taxonomic rank; update documentation of clean_duplicates and sugges_names_taxadb

--- R/bdc_clean_duplicates.R
+++ R/bdc_clean_duplicates.R
@@ -1,10 +1,13 @@
-#' Filter duplicated names in bdc_get_taxa_taxadb function.
+#' Filter duplicated names with multiple accepted names
 #'
-#' Function only to internal use of bdc_get_taxa_taxadb function.
+#' This function is used to flag and remove names with multiple accepted name (i.e., column 'taxonomicStatus').
 #' 
-#' @param data A data.frame created in bdc_get_taxa_taxadb.  
-#'
-#' @return Return the data.frame without duplicates. 
+#' @param data data.frame. Database exported from bdc_get_taxa_taxadb function.
+#' @param rank_name character string. A taxonomic rank used to be used to filter data. Options available are: "kingdom", "phylum", "class", "order", "family", and "genus".
+#' @param rank_name character string. A taxonomic rank used to be used to filter data. Options available are: "kingdom", "phylum", "class", "order", "family", and "genus". Default = NULL.
+#' @param rank character string. Taxonomic rank name (e.g. "Plantae", "Animalia", "Aves", "Carnivora". Default is NULL.
+#' @details For some taxonomic databases available in 'taxadb' package, columns ragarding taxonomic ranks (e.g., kingdom, phyllum ) are empty (or containg only few information). In such cases, the full database wil be used in the analyses. 
+#' @return A data.frame without duplicated accepted names and flags "|check +1 accepted" in the column 'notes'. 
 #' @export
 #'
 #' @examples
@@ -12,8 +15,7 @@ bdc_clean_duplicates <-
   function(data,
            rank = NULL,
            rank_name = NULL) {
-    # Filter all names except those flag as "accepted" (e.g.,  misapplied,
-    # heterotypic synonym, synonym, homotypic synonym, proparte synonym )
+    # Filter all names except those flag as "accepted" (e.g.,  heterotypic, homotypic, pro-parte synonyms, and doubtful )
     data <- data[order(data$taxonomicStatus), ]
     data <- data[!(duplicated(data$input) & data$taxonomicStatus != "accepted"), ]
     
@@ -21,7 +23,7 @@ bdc_clean_duplicates <-
     if (!is.null(rank_name) & !is.null(rank)) {
       valid_duplicates <-
         data[duplicated(data$input) & data$taxonomicStatus == "accepted", ] %>%
-        dplyr::filter(., .data[[rank]] == rank_name)
+        dplyr::filter(., .data[[rank]] == rank_name | is.na(.data[[rank]]))
     } else if (is.null(rank_name) & !is.null(rank)) {
       message("Please, provide both 'rank_name' and 'rank' arguments")
     } else if (!is.null(rank_name) & is.null(rank)) {
@@ -39,7 +41,8 @@ bdc_clean_duplicates <-
     if (length(uni_names) > 0) {
       for (i in 1:length(uni_names)) {
         index <- which(data$scientificName == uni_names[i])
-        data[index, "notes"] <- "|check +1 accepted"
+        data[index, 2:(ncol(data)-3)] <- NA 
+        data[index, "notes"] <- "multipleAccepted"
       }
     }
     
--- R/bdc_suggest_names_taxadb.R
+++ R/bdc_suggest_names_taxadb.R
@@ -1,21 +1,21 @@
-#' Finding the potential match candidate for misspelled names
+#' Finding a potential matching candidate for misspelled names
 #'
 #' This algorithm searches for a potential match candidate within an orthographic distance of each misspelled names. Only names within an orthographic distance specifyed in 'max_distance' will be returned. This function was built based on the 'suggest.name' function of the flora package.
 #'
-#' @param sci_name character, containing scientific to be searched in a taxonomic database available in the 'taxadb' package.
-#' @param max_distance numeric,  a value between 0 and 1 specifying the minimum distance between the scientific names and names suggested by a fuzzy matching. Values close to 1 indicate that only a few differences between scientific names and name suggested are allowed. Default is 0.9.
-#' @param provider A database where the valid and suggest names should be searched. The options are those provided by taxadb package.
-#' @param rank_name a character string of taxonomic scientific name (e.g. "Plantae"). Default is NULL
-#' @param rank a character string containing a taxonomic rank name (e.g. "kingdom"). Default is NULL
+#' @param sci_name character string. Scientific matched against names from a taxonomic database available in the 'taxadb' package.
+#' @param max_distance numeric. Value between 0 and 1 specifying the maximum distance between the scientific names and names suggested by a fuzzy matching. Values close to 1 indicate that only a few differences between scientific names and name suggested are allowed. Default = 0.9.
+#' @param provider character string. A database where the valid and suggest names should be searched. The options are those provided by the 'taxadb' package.
+#' @param rank_name character string. A taxonomic rank used to be used to filter data. Options available are: "kingdom", "phylum", "class", "order", "family", and "genus". Default = NULL.
+#' @param rank character string. Taxonomic rank name (e.g. "Plantae", "Animalia", "Aves", "Carnivora". Default is NULL.
 #' @param parallel logical, whether running in parallel. By default, it is TRUE
-#' @param ncores integer, number of cores to be used for parallel processing. By default, it is 2
-#' @return This function returns a data.frame whose first column is the suggested name and the second column is the distance between the sci_name and the suggested name.
-#' It is worth to note that if there are two names with equal distances, only the first one is returned.
+#' @param ncores numeric. Number of cores to be used for parallel processing. Default = 2.
+#' @details For some taxonomic databases available in 'taxadb' package, columns ragarding taxonomic ranks (e.g., kingdom, phyllum ) are empty (or containg only few information). In such cases, the full database wil be used in the analyses. 
+#' @return This function returns a data.frame whose first column is the suggested name and the second column is the distance between the sci_name and the suggested name. It is worth to note that if there are two names with equal distances, only the first one is returned.
 #' @export
 #'
 #' @examples
-#' bdc_suggest_names_taxadb(c("Cebus apela", "Puma concolar"), provider = "gbif")
-#'
+#' x <- ("Cebus apela", "Puma concolar")
+#' bdc_suggest_names_taxadb(x, provider = "gbif")
 bdc_suggest_names_taxadb <-
   function(sci_name,
            max_distance = suggestion_distance,
@@ -32,14 +32,14 @@ bdc_suggest_names_taxadb <-
       },
       USE.NAMES = FALSE
       ))
-
+    
     first_letter <- base::toupper(first_letter)
-
+    
     # Should taxonomic database be filter according to a taxonomic rank name?
     if (!is.null(rank_name) & !is.null(rank)) {
       species_first_letter <-
         taxadb::taxa_tbl(provider) %>%
-        dplyr::filter(., .data[[rank]] == rank_name) %>%
+        dplyr::filter(., .data[[rank]] == rank_name | is.na(.data[[rank]])) %>%
         dplyr::pull(scientificName) %>%
         grep(paste0("^", first_letter, collapse = "|"), ., value = TRUE)
     } else if (is.null(rank_name) & !is.null(rank)) {
@@ -52,19 +52,18 @@ bdc_suggest_names_taxadb <-
         dplyr::pull(scientificName) %>%
         grep(paste0("^", first_letter, collapse = "|"), ., value = TRUE)
     }
-
-
+    
     # Should parallel processing be used?
     if (parallel == TRUE) {
       # setup parallel backend to use many processors
       cl <- parallel::makeCluster(ncores) # not to overload your computer
       doParallel::registerDoParallel(cl)
-
+      
       sug_dat <-
         foreach(i = sci_name,
                 .combine = rbind, .export = "bdc_return_names") %dopar% {
-          bdc_return_names(i, max_distance, species_first_letter)
-        } # end foreach
+                  bdc_return_names(i, max_distance, species_first_letter)
+                } # end foreach
       parallel::stopCluster(cl) # stop cluster
     } else {
       sug_dat <-
@@ -73,7 +72,7 @@ bdc_suggest_names_taxadb <-
           suggested = character(length(sci_name)),
           distance = numeric(length(sci_name))
         )
-
+      
       for (i in seq_along(sci_name)) {
         sug_dat[i, ] <-
           bdc_return_names(sci_name[i], max_distance, species_first_letter)
