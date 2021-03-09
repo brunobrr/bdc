096ebcb332f28e5f734e76154f455a6cfb5da073
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2021-03-09 15:06:32 -0300

    update docs: missing_xy

--- R/bdc_missing_xy.R
+++ R/bdc_missing_xy.R
@@ -7,28 +7,35 @@
 #' "decimalLatitude".
 #' @param lon character string. The column with longitude. Default =
 #' "decimalLongitude".
+#'
 #' @details This test identifies records missing geographic coordinates (i.e.,
 #' empty or not applicable [NA] longitude or latitude)
+#'
 #' @return A data.frame contain the column '.missing_xy'. Records that have
 #' failed in the test are flagged as "FALSE".
 #'
+#' @importFrom dplyr mutate_all mutate case_when select bind_cols
+#' @importFrom rlang sym
+#' 
+#' @export
+#'
 #' @examples
+#' \dontrun{
 #' decimalLatitude <- c(19.9358, -13.016667, NA, "")
 #' decimalLongitude <- c(-40.6003, -39.6, -20.5243, NA)
 #' x <- data.frame(decimalLatitude, decimalLongitude)
-#' 
-#' bdc_missing_xy(data = x, lat = "decimalLatitude", lon =
-#' "decimalLongitude")
+#' bdc_missing_xy(data = x, lat = "decimalLatitude", lon = "decimalLongitude")
+#' }
 bdc_missing_xy <-
   function(data, lon = "decimalLongitude", lat = "decimalLatitude") {
     df <- data
-    
+
     suppressWarnings({
       df <-
         df %>%
         dplyr::mutate_all(as.numeric)
     })
-    
+
     df <-
       df %>%
       dplyr::mutate(
@@ -45,9 +52,9 @@ bdc_missing_xy <-
         )
       ) %>%
       dplyr::select(.missing_xy)
-    
+
     df <- dplyr::bind_cols(data,  df)
-    
+
     message(
       paste(
         "\nbdc_missing_xy:\nFlagged",
@@ -55,6 +62,6 @@ bdc_missing_xy <-
         "records.\nOne column was added to the database.\n"
       )
     )
-    
+
     return(df)
   }
