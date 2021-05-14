e8acee5e9249fe5b3e7d171ed5694447e8ecf1c7
Bruno Ribeiro <ribeiro.brr@gmail.com>
2021-05-14 07:33:49 -0300

    degun bdc_query_names_taxadb; changes made to link same suggested name to different original names; minor changes in the annotation

--- R/bdc_clean_duplicates.R
+++ R/bdc_clean_duplicates.R
@@ -31,7 +31,7 @@ bdc_clean_duplicates <-
     # homotypic, pro-parte synonyms, and doubtful)
     data <- data[order(data$taxonomicStatus), ]
     data <- data[!(duplicated(data$input) & data$taxonomicStatus != "accepted"), ]
-
+    
     # Filter out database according to a taxonomic rank
     if (!is.null(rank_name) & !is.null(rank)) {
       valid_duplicates <-
@@ -45,19 +45,19 @@ bdc_clean_duplicates <-
       valid_duplicates <-
         data[duplicated(data$input) & data$taxonomicStatus == "accepted", ]
     }
-
+    
     valid_duplicates <- valid_duplicates %>% dplyr::select(scientificName)
     data <- data[!duplicated(data$input), ]
     uni_names <- unique(valid_duplicates$scientificName)
-
+    
     # Add a flag to names with more than one accepted name found
     if (length(uni_names) > 0) {
       for (i in 1:length(uni_names)) {
         index <- which(data$scientificName == uni_names[i])
-        data[index, 2:(ncol(data)-3)] <- NA
+        data[index, 2:(ncol(data)-4)] <- NA
         data[index, "notes"] <- "multipleAccepted"
       }
     }
-
+    
     return(data)
   }
