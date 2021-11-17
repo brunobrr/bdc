3d9623a8661148939a089c109114dfa29f7a0b6f
sjevelazco <sjevelazoc@gmail.com>
2021-11-17 20:26:42 -0300

    new tests

---
+++ tests/testthat/test-bdc_coordinates_from_locality.R
@@ -0,0 +1,13 @@
+test_that("test based on function example", {
+
+  lat <- c(NA, NA, "")
+  lon <- c("", NA, NA)
+  x <- data.frame(lat = lat, lon = lon, locality =  c("Brazil", "Argentina", "Chile"))
+  
+  expect_message(bdc_coordinates_from_locality(data = x, lat = "lat", lon = "lon"))
+  
+  r <- bdc_coordinates_from_locality(data = x, lat = "lat", lon = "lon")
+  
+  expect_equal(dim(r), c(3,4))
+  
+  })
