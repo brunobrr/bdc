a0a0cd669e12a8ac5e8f4b5fffd9a6340a8cb120
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2021-12-03 08:07:29 -0300

    add tests for summary_col, year_from_eventDate, and year_outOfRange

---
+++ tests/testthat/test-bdc_year_outOfRange.R
@@ -0,0 +1,13 @@
+context("year out of range")
+
+collection_date <- c(NA, "31/12/2015", "2013-06-13T00:00:00Z", "2013-06-20", "", "2013", "0001-01-00")
+
+x <- data.frame(collection_date)
+
+test_that("bdc_year_outOfRange detect years out of range", {
+
+  resultant <- bdc_year_outOfRange(data = x, eventDate = "collection_date")
+
+  expect_equal(resultant$.year_outOfRange, c(TRUE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE))
+
+})
