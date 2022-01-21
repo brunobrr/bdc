9f32447fdea4d92a0fd57c9daddf0cc20f6f1cdf
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2022-01-21 18:32:39 -0300

    fix data mask test-bdc_coordinates_empty

--- tests/testthat/test-bdc_quickmap.R
+++ tests/testthat/test-bdc_quickmap.R
@@ -5,17 +5,14 @@ polygon.y = rnorm(100, 20, 30)
 group = rnorm(100, 50, 50)
 data <- data.frame(group, polygon.x, polygon.y)
 
-bdc_coordinates_empty(data, lat = polygon.y, lon = polygon.x)
-
+bdc_coordinates_empty(data, lat = "polygon.y", lon = "polygon.x")
 
 test_that("plot_quick_map", {
-  
+
     plot_test <-  bdc_quickmap(data, lon= "polygon.x", lat= "polygon.y")
-    
+
     x <- try(print(plot_test))
-    
+
     expect_equal(any(class(x) %in% "ggplot"), TRUE)
-    
+
 })
-
-
