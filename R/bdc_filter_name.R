ae508553ea5960993f054d2fff1e2e3486a4e610
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2022-02-01 15:23:59 -0300

    update namespace

--- R/bdc_filter_name.R
+++ R/bdc_filter_name.R
@@ -1,80 +1,80 @@
 #' Internal function. Filter species information from local stored taxonomic databases
-#' 
+#'
 #' This function filters, by exact match, species information from taxonomic database.
-#' 
-#' @param sci_name character vector. Containing scientific names. 
-#' @param db character vector. Local taxonomic database, eg. "gbif". 
+#'
+#' @param sci_name character vector. Containing scientific names.
+#' @param db character vector. Local taxonomic database, eg. "gbif".
 #' @param version character vector. The year of database version, eg. "2022". The default is the latest version.
-#' 
-#' @details 
-#' 
-#' The function looks for an exact match of scientific name in the local stored database and returns the matched information 
+#'
+#' @details
+#'
+#' The function looks for an exact match of scientific name in the local stored database and returns the matched information
 #' as a data.frame in Darwin Core format. Unmatched species return NA values for all taxonomic information.
-#' 
-#' 
-#' @return  This function returns a data.frame in Darwin Core format with the taxonomic information for the queried species. 
+#'
+#'
+#' @return  This function returns a data.frame in Darwin Core format with the taxonomic information for the queried species.
 #' @importFrom dplyr mutate collect right_join relocate sym
-#' @importFrom taxadb latest_version td_connect  
-#' @importFrom tibble tibble 
-#' @examples 
+#' @importFrom taxadb td_connect
+#' @importFrom tibble tibble
+#' @examples
 #' \dontrun{
 #' }
 
 bdc_filter_name <- function (sci_name, db, version = taxadb:::latest_version())
 {
- 
+
   x <- tolower(sci_name)
-  
-  db_tbl <- dplyr::mutate(taxadb::taxa_tbl(db, schema = "dwc", version, 
+
+  db_tbl <- dplyr::mutate(taxadb::taxa_tbl(db, schema = "dwc", version,
                                            taxadb::td_connect()), input = !!dplyr::sym("scientificName"))
-  
+
   species_tab <- tibble::tibble(input = x, sort = seq_along(x))
- 
-  
-  
+
+
+
   out <- dplyr::collect(dplyr::right_join(db_tbl, species_tab, by = "input", copy = TRUE))
-  
-  
+
+
   return(dplyr::relocate(out, sort, .before = taxonID))
-  
+
 }
 
 #' Internal function. Filter species information from local stored taxonomic databases
-#' 
+#'
 #' This function filters, by exact match, species information from taxonomic database based on a taxonomic code.
-#' 
-#' @param id character vector. Containing taxonomic code. 
-#' @param db character vector. Local taxonomic database, eg. "gbif". 
+#'
+#' @param id character vector. Containing taxonomic code.
+#' @param db character vector. Local taxonomic database, eg. "gbif".
 #' @param version character vector. The year of database version, eg. "2022". The default is the latest version.
-#' 
-#' @details 
-#' 
-#' The function looks for an exact match of taxonomic code in the local stored database and returns the matched information 
+#'
+#' @details
+#'
+#' The function looks for an exact match of taxonomic code in the local stored database and returns the matched information
 #' as a data.frame in Darwin Core format. Unmatched ids return NA values for all taxonomic information.
-#' 
-#' 
-#' @return  This function returns a data.frame in Darwin Core format with the taxonomic information for the queried ids. 
+#'
+#'
+#' @return  This function returns a data.frame in Darwin Core format with the taxonomic information for the queried ids.
 #' @importFrom dplyr mutate collect right_join relocate sym
-#' @importFrom taxadb latest_version td_connect  
-#' @importFrom tibble tibble 
-#' @examples 
+#' @importFrom taxadb td_connect
+#' @importFrom tibble tibble
+#' @examples
 #' \dontrun{
 #' }
 
 
 bdc_filter_id <- function (id, db, version = taxadb:::latest_version())
 {
-  
-  db_tbl <- dplyr::mutate(taxadb::taxa_tbl(db, schema = "dwc", version, 
+
+  db_tbl <- dplyr::mutate(taxadb::taxa_tbl(db, schema = "dwc", version,
                                            taxadb::td_connect()), input = !!dplyr::sym("taxonID"))
-  
+
   species_tab <- tibble::tibble(input = id, sort = seq_along(id))
-  
-  
-  
+
+
+
   out <- dplyr::collect(dplyr::right_join(db_tbl, species_tab, by = "input", copy = TRUE))
-  
-  
+
+
   return(dplyr::relocate(out, sort, .after = taxonID))
-  
+
 }
