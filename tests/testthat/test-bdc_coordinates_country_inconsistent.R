787a7d7cecad36aa29b23c43a2cc47b4df922c5f
sjevelazco <sjevelazoc@gmail.com>
2022-03-09 11:05:58 -0800

    bdc_coordinates_country_inconsistent was improved and a new argument was added

--- tests/testthat/test-bdc_coordinates_country_inconsistent.R
+++ tests/testthat/test-bdc_coordinates_country_inconsistent.R
@@ -1,9 +1,10 @@
 test_that("test with function example", {
   metadata <- readr::read_csv(system.file("extdata", "input_files/gbif.csv", package = "bdc"))
-
+  metadata$countryCode <- "Brazil"
   df <- bdc_coordinates_country_inconsistent(
     data = metadata,
     country_name = "Brazil",
+    country = "countryCode",
     lon = "decimalLongitude",
     lat = "decimalLatitude",
     dist = 0.1 # in decimal degrees
@@ -16,10 +17,11 @@ test_that("test with function example", {
   decimalLongitude <- c(-40.6003, -39.6, -20.5243, NA, -64.105)
   decimalLatitude <- c(19.9358, -13.016667, NA, "", -12.558)
   x <- data.frame(decimalLongitude, decimalLatitude)
-
+  x$coutnry_suggested <- "Brazil" 
   df <- bdc_coordinates_country_inconsistent(
     data = x,
     country_name = c("Brazil"),
+    country = "coutnry_suggested",
     lon = "decimalLongitude",
     lat = "decimalLatitude",
     dist = 10 # in decimal degrees
