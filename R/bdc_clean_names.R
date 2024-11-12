10c6a9af68a0b48ca0c3094f2d20d3b04ad95335
Bruno Ribeiro <ribeiro.brr@gmail.com>
2024-11-12 07:47:06 -0300

    minor changes in bdc_clean_names to return a message asking to the user to install gnpaser

--- R/bdc_clean_names.R
+++ R/bdc_clean_names.R
@@ -7,7 +7,7 @@ is_linux <- function() unname(Sys.info()["sysname"] == "Linux")
 
 test_gnparser_setup <- function(bin_full_path){
   
-  if (!exec_exists(bin_full_path)) message("GNparser is not installed in your machine. Please use 'bdc_install_gnparser' to install the programm")
+  if (!exec_exists(bin_full_path)) message("GNparser is not installed in your machine. Please follow [this tutorial](https://brunobrr.github.io/bdc/articles/help/installing_gnparser.html) to install gnparser")
 }
 
 check_gnparser_setup <- function(){
