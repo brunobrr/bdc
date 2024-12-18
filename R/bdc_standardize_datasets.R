756a553986e164a9da38d4b0c1523a6d8d180c65
Bruno Ribeiro <ribeiro.brr@gmail.com>
2024-12-18 10:35:14 -0300

    Restore files removed for CRAN submission after acceptance

--- R/bdc_standardize_datasets.R
+++ R/bdc_standardize_datasets.R
@@ -38,7 +38,9 @@
 #' into R. Regardless of the method chosen, the data frame with metadata needs
 #' to contain the following column names (this is a list of required column
 #' names; for a comprehensive list of column names following Darwin Core
-#' terminology
+#' terminology, see
+#' [here](https://brunobrr.github.io/bdc/articles/integrate_datasets.html)
+#'
 #' - `datasetName`: A short name identifying the dataset (e.g., GBIF)
 #'
 #' - `fileName`: The relative path containing the name of the input dataset
